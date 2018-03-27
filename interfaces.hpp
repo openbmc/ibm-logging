ed7292ba31a0b046d277624449e741d5d78e4564
Matt Spinler <spinler@us.ibm.com>
2018-03-27 14:25:12 -0500

    Find a policy entry and create the Policy object
    Find an entry in the policy table based on the error
    log entry's properties, and create and save the
    sdbusplus object for it.
    
    Change-Id: Ifc09059169b5faedb4eae74a76d59847d7b2868b
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- interfaces.hpp
+++ interfaces.hpp
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <com/ibm/Logging/Policy/server.hpp>
+
 namespace ibm
 {
 namespace logging
 {
+
+template <typename... T>
+using ServerObject = typename sdbusplus::server::object::object<T...>;
+
+using PolicyInterface = sdbusplus::com::ibm::Logging::server::Policy;
+using PolicyObject = ServerObject<PolicyInterface>;
 
 enum class InterfaceType
 {
