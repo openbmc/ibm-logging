a72cc65fa4b04688eb2bfd3afb2f14aea4aab9bb
Matt Spinler <spinler@us.ibm.com>
2018-05-09 15:52:40 -0500

    do not merge: Revert "Implement the DeleteAll interface"
    This reverts commit 2cfceb4623c83c04ebe49c5dcae7da877c1c5c8b.
    
    If InterfacesRemoved handling does work, then don't need this.
    
    Change-Id: I842b0e839faa464c49b818f11bd8c555736acd6e

--- interfaces.hpp
+++ interfaces.hpp
@@ -2,7 +2,6 @@
 
 #include <com/ibm/Logging/Policy/server.hpp>
 #include <xyz/openbmc_project/Object/Delete/server.hpp>
-#include <xyz/openbmc_project/Collection/DeleteAll/server.hpp>
 
 namespace ibm
 {
@@ -14,10 +13,6 @@ using ServerObject = typename sdbusplus::server::object::object<T...>;
 
 using DeleteInterface = sdbusplus::xyz::openbmc_project::Object::server::Delete;
 using DeleteObject = ServerObject<DeleteInterface>;
-
-using DeleteAllInterface =
-    sdbusplus::xyz::openbmc_project::Collection::server::DeleteAll;
-using DeleteAllObject = ServerObject<DeleteAllInterface>;
 
 using PolicyInterface = sdbusplus::com::ibm::Logging::server::Policy;
 using PolicyObject = ServerObject<PolicyInterface>;
