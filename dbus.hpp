90c7c29d2f6038504e8d239fc85d549241677d34
Matt Spinler <spinler@us.ibm.com>
2018-05-23 11:31:22 -0500

    Add getService and getAllProperties functions
    These will be used to get callout information from
    the inventory.
    
    Tested:  The functions work when called in future commit.
    
    Change-Id: Ib9af94263506041c353fa813167051559c30bcac
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- dbus.hpp
+++ dbus.hpp
@@ -37,5 +37,37 @@ using ObjectValueTree =
 ObjectValueTree getManagedObjects(sdbusplus::bus::bus& bus,
                                   const std::string& service,
                                   const std::string& objPath);
+
+/**
+ * Get the D-Bus service name for the object path and interface.
+ *
+ * Returns an empty string if the service can't be found.
+ *
+ * @param[in] bus - the D-Bus object
+ * @param[in] objPath - the D-Bus object path
+ * @param[in] interface - the D-Bus interface name
+ *
+ * @return string - the service name
+ */
+std::string getService(sdbusplus::bus::bus& bus, const std::string& path,
+                       const std::string& interface);
+
+/**
+ * Returns all properties on a particular interface on a
+ * particular D-Bus object.
+ *
+ * Returns an empty map if there are any failures.
+ *
+ * @param[in] bus - the D-Bus object
+ * @param[in] service - the D-Bus service name
+ * @param[in] objPath - the D-Bus object path
+ * @param[in] interface - the D-Bus interface name
+ *
+ * @return DbusPropertyMap - The map of property names to values
+ */
+DbusPropertyMap getAllProperties(sdbusplus::bus::bus& bus,
+                                 const std::string& service,
+                                 const std::string& objPath,
+                                 const std::string& interface);
 }
 }
