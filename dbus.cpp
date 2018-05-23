39de2e17c370aaae82fe2f555ac7a0588aee2d7a
Matt Spinler <spinler@us.ibm.com>
2018-05-23 11:31:22 -0500

    Add getService and getAllProperties functions
    These will be used to get callout information from
    the inventory.
    
    Tested:  The functions work when called in future commit.
    
    Change-Id: Ib9af94263506041c353fa813167051559c30bcac
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- dbus.cpp
+++ dbus.cpp
@@ -21,6 +21,13 @@ namespace ibm
 namespace logging
 {
 
+constexpr auto MAPPER_BUSNAME = "xyz.openbmc_project.ObjectMapper";
+constexpr auto MAPPER_PATH = "/xyz/openbmc_project/object_mapper";
+constexpr auto MAPPER_IFACE = "xyz.openbmc_project.ObjectMapper";
+constexpr auto PROPERTY_IFACE = "org.freedesktop.DBus.Properties";
+
+using namespace phosphor::logging;
+
 ObjectValueTree getManagedObjects(sdbusplus::bus::bus& bus,
                                   const std::string& service,
                                   const std::string& objPath)
@@ -37,6 +44,7 @@ ObjectValueTree getManagedObjects(sdbusplus::bus::bus& bus,
     {
         using namespace phosphor::logging;
         log<level::ERR>("Failed to get managed objects",
+                        entry("SERVICE=%s", service.c_str()),
                         entry("PATH=%s", objPath.c_str()));
     }
     else
@@ -45,6 +53,60 @@ ObjectValueTree getManagedObjects(sdbusplus::bus::bus& bus,
     }
 
     return interfaces;
+}
+
+std::string getService(sdbusplus::bus::bus& bus, const std::string& objPath,
+                       const std::string& interface)
+{
+    auto method = bus.new_method_call(MAPPER_BUSNAME, MAPPER_PATH, MAPPER_IFACE,
+                                      "GetObject");
+
+    method.append(objPath);
+    method.append(std::vector<std::string>({interface}));
+
+    auto reply = bus.call(method);
+    if (reply.is_method_error())
+    {
+        return std::string{};
+    }
+
+    std::map<std::string, std::vector<std::string>> response;
+    reply.read(response);
+
+    if (response.empty())
+    {
+        return std::string{};
+    }
+
+    return response.begin()->first;
+}
+
+DbusPropertyMap getAllProperties(sdbusplus::bus::bus& bus,
+                                 const std::string& service,
+                                 const std::string& objPath,
+                                 const std::string& interface)
+{
+    DbusPropertyMap properties;
+
+    auto method = bus.new_method_call(service.c_str(), objPath.c_str(),
+                                      PROPERTY_IFACE, "GetAll");
+    method.append(interface);
+    auto reply = bus.call(method);
+
+    if (reply.is_method_error())
+    {
+        using namespace phosphor::logging;
+        log<level::ERR>("Failed to get all properties",
+                        entry("SERVICE=%s", service.c_str()),
+                        entry("PATH=%s", objPath.c_str()),
+                        entry("INTERFACE=%s", interface.c_str()));
+    }
+    else
+    {
+        reply.read(properties);
+    }
+
+    return properties;
 }
 }
 }
