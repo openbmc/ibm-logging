9791a07c9649946428ea06bb8d3187bc340ecec5
Matt Spinler <spinler@us.ibm.com>
2018-05-23 13:18:55 -0500

    Create the Callout objects
    If a new error log has the association interface, and the
    inventory item it points to implements the Asset interface,
    create a Callout object for it and persist it.
    
    It will be persisted to a file like:
    /var/lib/ibm-logging/errors/N/callouts/M where N is an error
    log entry ID and M is the callout instance number.
    
    Tested: Verify new D-Bus objects for callouts.
    
    Change-Id: I90e9cf76edd7c2517de22cee64c3b979c482efa8
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

--- dbus.hpp
+++ dbus.hpp
@@ -12,8 +12,22 @@ namespace logging
 
 using DbusInterface = std::string;
 using DbusProperty = std::string;
+
+static constexpr auto forwardPos = 0;
+static constexpr auto reversePos = 1;
+static constexpr auto endpointPos = 2;
+using AssociationsPropertyType =
+    std::vector<std::tuple<std::string, std::string, std::string>>;
+
+static constexpr auto calloutPos = 0;
+static constexpr auto snPos = 1;
+static constexpr auto pnPos = 2;
+using Callouts = std::tuple<std::string, std::string, std::string>;
+using CalloutList = std::vector<Callouts>;
+
 using Value = sdbusplus::message::variant<bool, uint32_t, uint64_t, std::string,
-                                          std::vector<std::string>>;
+                                          std::vector<std::string>,
+                                          AssociationsPropertyType>;
 
 using DbusPropertyMap = std::map<DbusProperty, Value>;
 using DbusInterfaceMap = std::map<DbusInterface, DbusPropertyMap>;
